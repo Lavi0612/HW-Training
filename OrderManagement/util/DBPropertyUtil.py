fc6c54340eb8b88e8a2ae060823af893aff3cbce
Lavi0612 <146415643+Lavi0612@users.noreply.github.com>
2025-04-17 09:39:30 +0530

    Delete OrderManagement directory

--- OrderManagement/util/DBPropertyUtil.py
+++
@@ -1,8 +0,0 @@
-def get_connection_string(filename):
-    props = {}
-    with open(filename, 'r') as file:
-        for line in file:
-            if '=' in line:
-                key, val = line.strip().split('=', 1)
-                props[key] = val
-    return props
